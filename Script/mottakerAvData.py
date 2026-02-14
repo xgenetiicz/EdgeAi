f92e1767bd05096aa6291e11e5990b9ccd5800d5
xgenetiicz <gentiagron123@gmail.com>
2026-02-14 13:40:26 +0100

    tweaking changes to find the sweet spot of confidens

--- Script/mottakerAvData.py
+++ Script/mottakerAvData.py
@@ -46,8 +46,8 @@ def upload():
         img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
         # AI IDENTIFISERING (Automatisk)
-        # VIKTIG: Satt ned til 0.01 for å feilsøke bildekvalitet
-        results = model(img, verbose=False, conf=0.01) 
+        # VIKTIG: Satt ned til 0.05 for å feilsøke bildekvalitet
+        results = model(img, verbose=False, conf=0.05) 
         
         # Sjekk om vi fant noen 'TARGET_OBJECTS'
         found_interesting = False

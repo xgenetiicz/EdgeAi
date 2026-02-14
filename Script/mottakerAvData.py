96f789caa1bcc016242dfd97f44ed925715ea41f
xgenetiicz <gentiagron123@gmail.com>
2026-02-14 14:42:11 +0100

    Testing medium version with confidens on 0.5

--- Script/mottakerAvData.py
+++ Script/mottakerAvData.py
@@ -25,7 +25,7 @@ if not os.path.exists(SAVE_PATH):
 
 #AI - modell oppstart på RAM ved oppstart på Pironman 5 16GB RAM - Raspberry pi 5
 print("Initialiserer 'hjernen' (YOLO + OCR)")
-model = YOLO("yolov8l.pt") #Laster ned L-modellen for best accuracy
+model = YOLO("yolov8m.pt") #Laster ned M-modellen for best accuracy
 reader = easyocr.Reader(['en'], gpu=False)
 print("Systemet er nå klart for objekt identifikasjon!")
 

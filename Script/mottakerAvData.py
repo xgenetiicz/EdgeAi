32a0d7d0170bdcafc64e9cbf8b2ff49f50354745
xgenetiicz <gentiagron123@gmail.com>
2026-03-23 18:27:12 +0100

    perf: Added knowlegde learned and taken from tesseract branch with testing images with grayscale and filling holes in the letters, and also rezising the actual license plate for easier recognition

--- Script/mottakerAvData.py
+++ Script/mottakerAvData.py
@@ -121,8 +121,17 @@ try:
 
                 # Kjører EasyOCR BARE på det utklipte bildet - dette er for å spare
                 if image_to_read is not None and image_to_read.size > 0:
-                    # NY LOGIKK: allowlist tvinger EasyOCR til å bare gjette på STUB og TALL, ikke flagg og småbokstaver
-                    ocr_result = reader.readtext(image_to_read, allowlist='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+
+                    #-- VI TAR MED BILDEVASKINGEN FRA TESSERACT BRANCHEN SLIK AT VI TESTER DET MED EASYOCR---
+
+                    #vi tester med å forstørre bildet som fungerer bra ved test med tesseract
+                    image_to_read = cv2.resize(image_to_read, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+                    gray= cv2.cvtColor(image_to_read, cv2.COLOR_BGR2GRAY)
+
+                    _, binary_image = cv2.threshold(gray, 130, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+                    
+                    # allowlist tvinger EasyOCR til å bare gjette på STUB og TALL, ikke flagg og småbokstaver
+                    ocr_result = reader.readtext(binary_image, allowlist='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ')
                 else:
                     ocr_result = []
 
